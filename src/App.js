272fedabc950bdb00359f2d778f2749cc5f3f502
Taniax1 <monuddin.taniaomni@gmail.com>
2023-02-26 03:26:02 +0000

    starting layout

--- src/App.js
+++ src/App.js
@@ -6,10 +6,40 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Welcome!!!!!!!!!!!!!
+          Tania
         </p>
       </header>
+      <header className="App-about">
+        <p>
+          About me
+        </p>
+        <div class="row">
+          <div class="about">
+            <p>aaaaaaaaaaaaaaaaaaaa</p>
+            <p>xxxxxxxxxx</p>
+          </div>
+          <div class="about">
+            <p>I am an explorer at heart, constantly fascinated by the complexity of nature and the boundless potential of technology. The power to create something new from nothing has always captivated me.</p>
+            <p>I believe that technology can be a transformative force for good, offering solutions to some of the world's most pressing problems. While my interests and passions are constantly evolving, I am driven by a deep desire to make a positive impact on the world - be it through engineering, design, or other creative pursuits, I am committed to finding new ways to use my skills and knowledge to create a better future.</p>
+            <p>My thirst for exploration and discovery is what drives me, whether it's through building something from scratch, tackling new challenges, or engaging with diverse perspectives, I relish every moment as a chance to discover something new. I believe that life is about the journey, not just the destination, and that every step we take can lead us to unexpected and amazing places.</p>
+          </div>
+        </div>
+      </header>
+      <header className="App-projects">
+        <p>
+          My portfolio
+        </p>
+      </header>
+      <header className="App-connect">
+        <p>
+          Get in contact
+        </p>
+      </header>
+      <footer class="footer">
+        tania
+      </footer>
     </div>
+    
   );
 }
 
