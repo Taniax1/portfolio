4411db41adda3b931a8c194ad47572e2b8ee8cfb
Taniax1 <monuddin.taniaomni@gmail.com>
2023-06-12 23:13:58 +0100

    Updated

--- src/App.js
+++ src/App.js
@@ -4,26 +4,127 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <p>aaaa</p>
       <header className="App-header">
-        <p>
-          Tania
-        </p>
-      </header>
-      <header className="App-about">
-        <p>
-          About me
-        </p>
-        <div class="row">
-          <div class="about">
-            <p>aaaaaaaaaaaaaaaaaaaa</p>
-            <p>xxxxxxxxxx</p>
-          </div>
-          <div class="about">
-            <p>I am an explorer at heart, constantly fascinated by the complexity of nature and the boundless potential of technology. The power to create something new from nothing has always captivated me.</p>
-            <p>I believe that technology can be a transformative force for good, offering solutions to some of the world's most pressing problems. While my interests and passions are constantly evolving, I am driven by a deep desire to make a positive impact on the world - be it through engineering, design, or other creative pursuits, I am committed to finding new ways to use my skills and knowledge to create a better future.</p>
-            <p>My thirst for exploration and discovery is what drives me, whether it's through building something from scratch, tackling new challenges, or engaging with diverse perspectives, I relish every moment as a chance to discover something new. I believe that life is about the journey, not just the destination, and that every step we take can lead us to unexpected and amazing places.</p>
+        <div className="name-contour">
+          <div class="name">
+            Tania Monuddin
           </div>
         </div>
+
+
+
+        <div class="board">
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+  </div>
+
+
+
+      </header>
+
+
+
+      <header className="App-about">
+      <br /><br /><br />
+      <div class="container">
+
+<div class="row">
+  <div class="col-sm-7">
+    <div class="about">
+              <h2>About me</h2><br />
+              <p>I am an explorer at heart, constantly fascinated by the complexity of nature and the boundless potential of technology. The power to create something new from nothing has always captivated me.</p>
+              <p>I believe that technology can be a transformative force for good, offering solutions to some of the world's most pressing problems. While my interests and passions are constantly evolving, I am driven by a deep desire to make a positive impact on the world - be it through engineering, design, or other creative pursuits, I am committed to finding new ways to use my skills and knowledge to create a better future.</p>
+              <p>My thirst for exploration and discovery is what drives me, whether it's through building something from scratch, tackling new challenges, or engaging with diverse perspectives, I relish every moment as a chance to discover something new. I believe that life is about the journey, not just the destination, and that every step we take can lead us to unexpected and amazing places.</p>
+            </div>
+            </div>
+  {/* <div class="col-sm-1"></div>
+  <div class="col-sm-3">
+    <br /> <br /> 
+    <div class="about2">
+      <h3>Software Development</h3>
+        <h3>Software Design</h3>
+        <h3>Machine Learning</h3>
+        <h3>Data Science</h3>
+    <br /> <br /> <br />
+        </div>
+  </div> */}
+</div>
+</div>
+
+<br /><br /><br />
+<div class="container">
+
+<div class="row">
+  <div class="col-sm-7"></div>
+  <div >
+    <div class="about">
+              <h2>Interests</h2>
+      <div class="row"><div class="col-sm-9  col-lg-8"></div><div class="col-sm-3  col-lg-4"><h3>Software Engineering and Development</h3></div></div><br />
+      <div class="row"><div>
+        <h3>Software Design</h3>
+        <ul>
+          <li></li>
+        </ul>
+      </div></div><br />
+        <div class="row"><div class="col-sm-6  col-lg-8"></div><div class="col-sm-6 col-lg-4"><h3>Machine Learning</h3></div></div><br />
+        <div class="row"><div><h3>Data Science</h3></div></div><br />
+        <div class="row"><div class="col-lg-8"></div><div class="col-lg-4"><h3>Data Science</h3></div></div></div>
+            </div>
+</div>
+</div>
+
+
+
+
+
+
+{/* <div class="interests">
+              <h2>Interests</h2><br />
+      <h3>Software Engineering</h3>
+      <h3>Software Development</h3>
+        <h3>Software Design</h3>
+        <h3>Machine Learning</h3>
+        <h3>Data Science</h3>
+      </div> */}
+            
+
+
+
+
+
+
+
+        {/* <div class="side-by-side"> */}
+
+
+          
+          {/* <div class="col-md-12 col-lg-7">
+
+          <div class="col-md-6 col-lg-12">
+            <div class="about">
+              <h2>About me</h2><br />
+              <p>I am an explorer at heart, constantly fascinated by the complexity of nature and the boundless potential of technology. The power to create something new from nothing has always captivated me.</p>
+              <p>I believe that technology can be a transformative force for good, offering solutions to some of the world's most pressing problems. While my interests and passions are constantly evolving, I am driven by a deep desire to make a positive impact on the world - be it through engineering, design, or other creative pursuits, I am committed to finding new ways to use my skills and knowledge to create a better future.</p>
+              <p>My thirst for exploration and discovery is what drives me, whether it's through building something from scratch, tackling new challenges, or engaging with diverse perspectives, I relish every moment as a chance to discover something new. I believe that life is about the journey, not just the destination, and that every step we take can lead us to unexpected and amazing places.</p>
+            </div>
+          </div>
+
+          <div class="about">
+            <div>
+              <p>aaaaaaaaaaaaaaaaaaaa</p>
+              <p>xxxxxxxxxx</p>
+            </div>
+          </div>
+          </div> */}
+        {/* </div> */}
       </header>
       <header className="App-projects">
         <p>
@@ -32,11 +133,33 @@ function App() {
       </header>
       <header className="App-connect">
         <p>
-          Get in contact
+          Feel free to reach out!
         </p>
+    <br />
+
+        <div class="container">
+
+<div class="row">
+  <div class="col-sm-4">
+    <div class="about">
+      <h2>About me</h2><br />
+    </div>
+  </div>
+  <div class="col-sm-1"></div>
+  <div class="col-sm-7">
+    <div class="about">
+      <h3>Email</h3>
+    <br /> <br /> <br />
+        </div>
+  </div>
+</div>
+</div>
+
+
+
       </header>
       <footer class="footer">
-        tania
+        **footer**
       </footer>
     </div>
     
